4bdd8586c5365bbcbc45b87242c9a675b2aee092
Shavkat Berdibekov <103271898+shavkat9@users.noreply.github.com>
2022-08-06 16:54:29 +0500

    myHistory calculator
    time calculates life

---
+++ js/app.js
@@ -0,0 +1,801 @@
+// // let points = 45;
+
+// // let res = points >= 70 ? 'utdingiz' : 'utmadingiz'
+
+// // console.log(res);
+
+// let points = 45;
+ 
+
+// // function points (num)
+// // if (points < 46){
+// //     console.log('ok')
+// // }
+// // else{
+// //     console.log('bad')
+// // }
+// // // console.log('points')
+
+// // // // let element = document.querySelector('element').innerHTML = 'salom hey' 
+
+// // // // let li = document.getElementById('he').innerHTML = 'alixon'
+
+// // // // let p = document.getElementById('itim').innerHTML = 'lorem5'
+
+
+// // // // let h5 = document.querySelector('h5').innerHTML ='Hello Muhammajon'
+
+// // // let arr = ['shavkat', 'xolboy', 'samad', 'xolliy'  ]
+// // // console.log(arr.includes('xolliy'))
+// // // console.log(arr).toU
+
+// // // let h5 = document.querySelector('title').innerHTML = 'js download'
+
+// // // let variableNmae = 'Shavkat'
+// // // let lastname = 'Berdibekov'
+// // // fullName = variableNmae.includes('t')
+// // // console.log(fullName)
+
+// // const items = [
+// //     { name: 'Bike', price: 100 },
+// //     { name: 'TV', price: 200 },
+// //     { name: 'Album', price: 10 },
+// //     { name: 'Book', price: 5 },
+// //     { name: 'Phone', price: 500 },
+// //     { name: 'Computer', price: 1000 },
+// //     { name: 'Keyboard', price: 25 },
+// // ]
+// // const itttttm = items.forEach((item) => {
+// //     console.log( item.price)
+// // })
+// let ism = prompt('Ismingizni kiriting:')
+
+// let birinchi = ism.charAt().toUpperCase()
+// let ikkinchi = ism.slice(2).toLocaleLowerCase()
+// let res = birinchi + ikkinchi
+
+// alert(`Salom ${res}`)
+
+// let guests = ['sardor', 'doniyor', 'husan']
+
+// // for(let i = 0; i < guests.length; i++){
+
+// //     console.log(guests[i].)
+// // }
+
+
+// let mehmon = ['aziz', 'xolboy', 'sodiq']
+
+// let i = 0;
+
+// while(i < mehmon.length){
+//     console.log(mehmon[i].toUpperCase())
+//     i++
+// }
+
+// let title = document.querySelector('title').innerHTML = ' Subline Text'
+
+
+// let dust = ['xomid', 'salim', 'sodiq']
+
+
+// let i = 0;
+
+// do{
+//     console.log(i)
+//     i++
+    
+// }while (i < 5)
+
+// let age = 25
+
+// if(age > 20) {
+//     console.log('siz 20 yoshdan kattasiz')
+
+// let guests = ['aziz', 'xolboy', 'doston']
+
+// if(guests.length > 3) {
+//     console.log('juda kop maexmon bor')
+// }else{
+//     console.log('Munchalik mexmon kam endi')
+// // }
+
+// // let parol = "pasffffffffffffffsw4"
+
+// // if(parol.length >  8){
+// //     console.log('siz koproq parol kiitdingi')
+
+// // }else{
+// //     console.log('muncha kam parol kiritdiingiz')
+// // }
+
+
+// // let user = false;
+
+// // if(!user){
+// //     console.log('Hech qanday foydalanuvchi yoq')
+// // }
+
+// // let natija = [100, 45, 84, 93, 81, 93, 86, 94, 99]
+
+// // for(let i = 0; i < natija.length; i++){
+// //     console.log("Sizning natijangiz", natija[i])
+
+// //     if(natija[i] === 81){
+// //         console.log("siz zo'r ball topladiz")
+// //         break
+// //     }
+// // }
+
+// // for(let i = 0; i < natija.length; i++){
+// // //     console.log("sizi balliz", natija[i])
+
+// // //     if(natija[i] === 45){
+// // //         console.log("sizi baliz zorku", natija[i])
+// // //         continue
+// // //     }
+
+// // // }
+
+// // const mark = 'm'
+
+
+// // switch (mark){
+// //     case "A":
+// //         console.log('siz A baho oldingiz')
+// //         break
+// //     case "B":
+// //         console.log('siz B baho oldingiz')
+// //         break
+// //     case "C":
+// //         console.log('siz C baho oldingiz')
+// //         break
+// //     case "D":
+// //         console.log('siz D baho oldingiz')
+// //         break
+// //     default:
+// //         console.log('sizda bho yoq')
+// // }
+// //  let joy = 'fargona'
+// // {
+// //     // let joy = 'Toshkent';
+
+// //     {
+// //         console.log(joy)
+// //     }
+// // }
+
+// // for(let i = 0; i < 10; i++){
+// //     if(i % 2 !== 0)
+// //     console.log(i)
+// // }
+// let i = 0;
+
+// while(i < 10){
+//     if(i % 2 !== 0)
+//     console.log(i)
+//     i++
+// }
+// let i = 0;
+
+// do{
+//     if(i % 2 !== 0)
+//     console.log(i)
+//     i++
+// }while(i < 10)
+// function suz  (name) {
+//     console.log(`Hayrli kun ${name}`)
+// }
+
+// suz('Shavkat')
+
+
+// const birinchi = function(name){
+//     console.log(`hayrli kun ${name}`)
+// }
+// birinchi('Shavkat')
+
+// const suz1 = (name) =>{
+//     console.log(`Hayrli kun ${name}`)
+// }
+// suz1('xolboy')
+
+// let names = ['shavkat', 'xolboy', 'muhammadjon', 'ehromjon', 'shuhratbek']
+
+// let bigName = []
+
+// names.forEach(function (name, i) {
+//     const newName = name.charAt().toUpperCase() + name.slice(1).toLowerCase() + 'bek'
+//     bigName.push(newName)
+
+//     console.log(newName, i)   
+// })
+
+// const element = document.querySelector('element')
+// console.log(element)
+
+// let lilar = document.getElementsByClassName('.id')
+// console.log(lilar)
+
+
+//  let helo = document.getElementsByTagName('h5')
+//  console.log(helo)
+
+// const itim = document.getElementById('itim')
+// console.log(itim)
+// let hovo = document.getElementsByClassName('hovo')
+// console.log(hovo)
+
+
+// let para = document.querySelectorAll('.List-Item')
+// para.forEach((para) =>{
+//     para.textContent += ' darslari'
+// })
+
+// let link = document.querySelector('a')
+// console.log(link.getAttribute('href'))
+
+// link.setAttribute('href', 'http:/')
+// link.textContent = 'Go to yandex google com'
+
+
+// let link = document.querySelector('ul')
+
+// console.log(link.getAttribute('class'))
+
+// console.log(link.setAttribute('class', 'booof'))
+
+
+// let limk = document.querySelector('h3')
+
+// // limk.setAttribute('style', 'color: red')
+
+// limk.style.color = 'yellow'
+// limk.style.backgroundColor = 'black'
+// limk.style.margin = '50px'
+// limk.style.color = '#ccc'
+// limk.style.alignItems = 'center'
+// limk.style.color = 'green'
+// limk.style.border = 'red'
+// limk.style.backgroundColor = '#ccc'
+
+
+// let myArray = ['true', [1, 2], 99, 'yaxshi']
+
+// // for(let i = 0; i < myArray.length; i++ ){
+
+// // if(typeof myArray[i] == 'string'){
+// //     console.log('String index:', i)
+// // }
+
+// // }
+
+// myArray.forEach((item, i) => {
+//     if(typeof myArray[i] == 'number'){
+//             console.log('number index:', i)
+//     }
+
+// })
+// myArray.forEach((item, i) => {
+//     if(typeof myArray[i] == 'boolean'){
+//             console.log('boolean index:', i)
+//     }
+
+// })
+
+// let data = [[1, 2, 3], true, false[4, 5, 6], 'yaxshi', 'yomon']
+
+// let newData = []
+
+// data.forEach((item)=>{
+//     const res = Array.isArray(item)
+//     if(res ){
+//         item.forEach((num) =>{
+//             newData.push(num)
+//         })
+//     }
+// })
+// console.log(newData)
+
+// let arra1 = [null, 'false', 0, 99, false, undefined, '', null, '0' ]
+
+// let res = []
+
+// arra1.forEach((item)=>{
+//     if(!item)
+//     res.push(item)
+// })
+
+// console.log(res)
+
+// let word = 'namangan'
+
+//  let res = word.split('').reverse()
+//  let newWord = ''
+
+//  for(let i = 0; i< res.length; i++){
+//     newWord += res[i]
+//  }
+
+//  console.log(newWord)
+// let suz = ['samarqand,', 0, '', ]
+
+// suz.for
+
+// let word1 = 'ASsaLOMu ALEkum YAxshIMisiZ ?'
+
+// let res = word.split(' ')
+// let newData = ''
+
+// res.forEach((item) => {
+//     let boshi = item.charAt().toUpperCase()
+// //     let oxiri = item.slice(1).toLocaleLowerCase()
+// //     newData += ' ' +  boshi + oxiri
+// // })
+// // console.log(newData)
+// let words = 'ASsaLOMu ALEkum YAxshIMisiZ ?'
+// let splitWord = words.split(' ')
+// let counter = 0
+// splitWord.forEach((word) => {
+//     if(word == 'a' || word == 'A' ){
+//      counter++   
+//     }
+    
+// })
+// console.log(`Suralyotgan suz bizda: ${counter} marta ishtirok etgan`)
+
+// let types = ['Roziya', 23, 93, ['i am Array'], null, undefined, 'array']
+// let type1 = []
+// types.forEach((type) =>{
+//     type1.push(typeof type)
+
+// })
+// console.log(type1)
+
+// let conaaaas = (1, 2)
+// if ( 1 <= 2){
+//     console.log('xush kelibsiz')
+// }
+// else{
+//     console.log('qani endi')
+// }
+
+// let word = 'jizzax'
+// let word1 = ''
+
+//  let res = word.split('').reverse()
+//  for(let i = 0; i < word.length; i++){
+//  word1 += res[i]
+
+//  }
+ 
+//  console.log(word1)
+
+// let data = [null, 33, 92, 'false', 'Roziya',  true, 'boolean', 09 ]
+
+// let fas = []
+
+// data.forEach((item) =>{
+//     if(typeof item ){
+//         fas.push(item)
+        
+
+
+//     }
+// })
+// console.log(fas)
+// =========================================================
+
+// let number555 = fizzBuzz('3')
+// console.log(number555)
+
+// function fizzBuzz(number){
+//     if(number % 3 === 0 && number % 5 === 0){
+//         console.log('fizzBuzz')
+//     }
+//     else if(number % 5 === 0){
+//         console.log('Buzz')
+//     }
+//     else if(number % 3 === 0){
+//         console.log('fizzBuzz')
+//     }
+//     else{
+//         console.log('son emas')
+//     }
+// }
+
+
+// for(let i = 0; i < 10; i+=1){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log('fizzBuzz')
+//     }
+//     else if(i % 5 === 0){
+//         console.log('Buzz')
+//     }
+//     else if(i % 3 === 0 ){
+//         console.log('fizz')
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+// =========================================================
+
+
+// let word = fizzBuzz('40')
+// console.log(word)
+
+// function fizzBuzz(num){
+//     if(num % 3 === 0 && num % 5 === 0){
+//         return 'fizzBuzz'
+//     }
+//     else if(num % 5 === 0){
+//         return 'buzz'
+//     }
+//     else if(num % 3 === 0){
+//         return 'fizzBuzz'
+//     }
+//     else if(typeof num !== 'number'){
+//         return 'son emas'
+//     }
+//     else{
+//         return 'nima bu'
+//     }
+// }
+// for(let i = 0; i < 10; i+=1){
+
+//     if( i % 3 === 0 && i % 5 === 0){
+//         console.log('fizzBuzz')
+//     }
+//     else if(i % 5 === 0){
+//         console.log('buzz')
+//     }
+//     else if( i % 3 === 0){
+//         console.log('fizzBuzz')
+//     }
+//     else{
+//         console.log('i')
+//     }
+// }
+
+// let num = 'i'
+// console.log('i')
+// for(let i = 0; i < 10; i+=1){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log('fizzBuzz')
+//     }
+//     else if(i % 5 === 0){
+//         console.log('Buzz')
+//     }
+//     else if(i % 3 === 0 ){
+//         console.log('fizz')
+//     }
+//     else{
+//         console.log(i)
+//     }
+// }
+
+// let num = fizzBuzz(3)
+
+// function fizzBuzz(num1){
+//     if(num1 % 3 === 0 && num1 % 5 === 0){
+//                 console.log('fizzBuzz')
+//             }
+//             else if(num1 % 5 === 0){
+//                 console.log('Buzz')
+//             }
+//             else if(num1 % 3 === 0 ){
+//                 console.log('fizz')
+//             }
+//             else{
+//                 console.log('nima bu!')
+//             }
+
+// }
+
+// let suz = 'jizzax'
+// let word = ''
+// let tobe = suz.split('').reverse()
+// for(let i = 0; i < suz.length; i++){
+//     word += tobe[i]
+// }
+// console.log(word)
+// let word = 'jizzax'
+// let word1 = ''
+
+//  let res = word.split('').reverse()
+//  for(let i = 0; i < word.length; i++){
+//  word1 += res[i]
+
+//  }
+ 
+//  console.log(word1)
+
+// let age = 5
+
+// if(age > 20 && age < 30){
+//     console.log('siz ancha yoshsiz')
+// }
+// else if(age < 20){
+//     console.log('siz qarisiz')
+// }
+// else{
+//     console.log('siz notogri raqam terdingiz ')
+// }
+
+// let Shavkatobj ={
+//     name: 'Shavkat',
+//     lastName: 'Berdibekov',
+//     age: 32,
+//     location: 'Jizzakh',
+//     isMarried: 'true',
+//     haqida: function(){
+//         return` Men ${name} ${lastName} hali ${age}daman ${this.isMarried ? 'uylanganman :': 'uylanmaganman'}
+//         `
+//     },
+//     let halala = haqida.Shavkatobj
+    
+    
+
+// }
+// console.log(obj)
+
+// let selectedColors = ['oq', 'yashil', 'kdkkd']
+// let soz = []
+// selectedColors.slice('').reverse()
+// soz.push(selectedColors)
+// console.log(soz)
+// let content = document.querySelector('ul')
+
+// console.log(content.classList)
+
+// content.classList.add('success')
+// content.classList.remove('success')
+
+// let para = document.querySelectorAll('p')
+// para.forEach((text) => {
+//     if(text.textContent.includes('success')){
+//       text.classList.add('sucess')
+//     }
+//     if(text.textContent.includes('error')){
+//         text.classList.add('error')
+//     }
+// })
+// let tog = document.querySelector('h3')
+
+// tog.classList.toggle('success')
+// tog.classList.toggle('success')
+// tog.classList.toggle('success')
+// tog.classList.toggle('success')
+
+// let para = document.querySelector('main')
+
+// // console.log(para.children)
+// Array.from(para.children).forEach((child) =>{
+//     child.classList.add('birba-lo')
+// })
+
+// let btn = document.querySelector('button')
+
+// btn.addEventListener('click', () => {
+//     console.log('you are clicked me!')
+
+// })
+
+// let con = document.querySelectorAll('p')
+
+// con.forEach(( item) =>{
+//   item.addEventListener('click',()=> {
+//  console.log('item clicked')
+
+//   })
+// })
+// let soz = 'salam, halam, malam'
+// let kala = []
+// for ( let i = 0; i < soz.length; i++){
+//   let suz = soz.split('').forEach(()=>{
+//   kala.push(suz)
+//   })
+// }
+
+
+// console.log(soz)
+// let main1 = document.querySelector('p')
+
+// let btn = document.querySelector('button')
+
+// btn.addEventListener('click', ()=> {
+//   // main1.innerHTML += '<li>Something is special</li>'
+// let li = document.createElement('p')
+//  li.textContent = 'someting add please'
+//  main1.appendChild(li)
+
+// })
+
+// const array1 = [1, 2, 3, 4, 5]
+
+// const map1 = array1.map((x)=>{
+//     x * 2
+// });
+// console.log(map1)
+
+// let suz = 'jizzax'
+
+// let res = suz.split(' ').reverse()
+// let res1 = ''
+// for(let j = 0; j < suz.length; j++){
+//   res1 += res[j]
+// } 
+
+// console.log(res1)
+
+// let num = (a, b)
+// console.log(num)
+
+//  function greatNum(){
+//     if(a < b){
+//         console.log('ha')
+//     }else{
+//         console.log('yooo')
+//     }
+//  }
+
+// console.log(true, false, 'true', 'false')
+
+// let email = 'shavkatberdiibekov@gmail.com'
+
+// let res = email.includes('s')
+// // console.log(res)
+
+// let age = 45
+
+// if(age == 28){
+//     console.log('true')
+// }else if (age > 30 && age < 40) {
+//     console.log('false')
+// }else{
+//     console.log('nima bu')
+// }
+
+// let ism = prompt('ISmingizni kiriting')
+
+
+
+
+// let ism1 = ism.charAt().toUpperCase()
+// let ism2 = ism.slice(1).toLocaleLowerCase()
+//  let ismlar = ism1 + ism2
+// alert(`salom,  ${ismlar}`)
+
+
+// const ism = [3, 56, 24, 16, 04, 100, 97, 93]
+
+// for(let i = 0; i < ism.length; i++){
+    
+// }
+// if(ism === 97){
+//     console.log('hala', [i])
+// }
+
+// function helloFunc(){
+//     console.log('hello func declaration')
+// }
+// helloFunc()
+// helloFunc()
+// helloFunc()
+
+// let ickkkon = function(){
+
+//     console.log('bye bye func expression')
+// }
+// ickkkon()
+// ickkkon()
+// ickkkon()
+// const func1 = () => {
+
+
+//  console.log('bye bye func')
+// }
+// func1()
+// func1()
+// func1()
+
+// let ala = ['qani', 'sani', 'mani', 'tani']
+
+// ala.forEach(function(e){
+//     let kalla = e.charAt().toUpperCase() + e.slice(1) .toLocaleString() + 'bek'
+//  console.log(kalla)
+
+// })
+// let sum = 40
+// if (sum >= 43) {
+//     console.log('nima kk')
+// }else  {
+//     console.log('qan kedi')
+// }
+
+// let a = 5;
+// let b = a;
+// a = 10;
+// console.log(a, b)
+
+
+// function fizzBuzz(){
+//     for(let i = 0; i < 100; i++){
+//         if(i % 3 === 0 && i % 5 === 0){
+//             console.log('fizzBuzz')
+//         }else if(i % 5 === 0){
+//             console.log('buzz')
+//         }else if(i % 3 === 0){
+//             console.log('fizz')
+//         }else{
+//             console.log(i)
+//         }
+//     }
+// }
+// fizzBuzz()
+
+
+// function getMax(a, b){
+//     // if(a > b){
+//     //     return a;
+//     // }else if(a < b){
+//     //     return b;
+//     // }else{
+//     //     return c;
+//     // }
+// return (a < b) ? 100 : 300
+
+// }
+// let maxNum = getMax(100, 300)
+// console.log(maxNum)
+
+// let name1 = 'Shavkat '
+// let surname = 'Berdibekov'
+
+// let fullName = name1.concat(surname)
+
+// console.log(fullName)
+// let errorParagraph = document.getElementById('error')
+// console.log(errorParagraph)
+// function purchase(){
+//     console.log('button clicked')
+
+//     errorParagraph.textContent = ' nimalar bolib ketdi hey'
+// }
+
+
+
+
+
+
+
+
+
+
+// <<<<<<---------myMemory practise------>>>>>
+
+let num1 = 8
+let num2 = 2
+
+document.getElementById('num1-el').textContent = num1
+document.getElementById('num2-el').textContent = num2
+
+
+let sumEl = document.getElementById('sum-el')
+function Add(){
+    let res = num1 + num2
+   sumEl.textContent = 'Sum:' + res
+}
+function Subtract(){
+    let res = num1 - num2
+   sumEl.textContent = 'Sum:' + res
+}
+function Divide(){
+    let res = num1 / num2
+   sumEl.textContent = 'Sum:' +res
+}
+function Multiply(){
+    let res = num1 * num2
+   sumEl.textContent = 'Sum:' + res
+}
